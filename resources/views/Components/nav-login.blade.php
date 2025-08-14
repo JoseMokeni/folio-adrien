77668fcba36164f6ec0cbebecea1bdaa85bbe5b9
Paul <adrypaul38@gmail.com>
2025-08-14 13:42:53 +0100

    design improvment

--- resources/views/Components/nav-login.blade.php
+++ resources/views/Components/nav-login.blade.php
@@ -4,50 +4,57 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Registration</title>
-    <script src= "https://cdn.tailwindcss.com"></script>
+    <title>Login</title>
+    <!-- Styles / Scripts -->
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @else
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <script src="{{ asset('js/app.js') }}" defer></script>
+    @endif
 </head>
-<body>
-
-<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-  <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-    <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" class="mx-auto h-10 w-auto" />
-    <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">{{ $slot }}</h2>
-  </div>
-
-  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <form action="" method="POST" class="space-y-6">
-        @csrf
-      <div>
-        <label for="email" :value = "old ('email')" class="block text-sm/6 font-medium text-gray-900">Email address</label>
-        <div class="mt-2">
-          <input id="email" type="email" name="email" required autocomplete="email" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+<body class="min-h-screen flex items-center justify-center bg-gray-50">
+    <div class="px-6 py-12 w-full max-w-lg content-center rounded-xl shadow-xl">
+        <div class="w-full">
+            <img src="/images/image.png" alt="Your Company" class="mx-auto h-10 w-auto" />
+            <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">{{ $slot }}</h2>
         </div>
-      </div>
 
-      <div>
-        <div class="flex items-center justify-between">
-          <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
-          <div class="text-sm">
-            <a href="/register" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
-          </div>
+        <div class="mt-10 w-full">
+            <form action="" method="POST" class="space-y-6">
+                @csrf
+                <div>
+                    <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
+                    <input id="email" type="email" name="email" required autocomplete="email"
+                        class="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+
+                    <label for="password" class="block text-sm/6 font-medium text-gray-900 mt-4">Password</label>
+                    <input id="password" type="password" name="password" required autocomplete="current-password"
+                        class="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+                </div>
+
+                <div class="flex items-center justify-between mt-2">
+                    <div></div>
+                    <div class="text-sm">
+                        <a href="/register" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+                    </div>
+                </div>
+
+                <div class="mt-6">
+                    <button type="submit"
+                        class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+                </div>
+            </form>
+
+            <p class="mt-10 text-center text-sm/6 text-gray-500">
+                Don't have an account?
+                <a href="/auth/register" class="font-semibold text-indigo-600 hover:text-indigo-500">Register</a>
+            </p>
+            <p class="mt-2 text-center text-sm/6 text-gray-500">
+                Not a member?
+                <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Start a 14 day free trial</a>
+            </p>
         </div>
-        <div class="mt-2">
-          <input id="password" type="password" name="password" required autocomplete="current-password" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
-        </div>
-      </div>
-
-      <div>
-        <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
-      </div>
-    </form>
-
-    <p class="mt-10 text-center text-sm/6 text-gray-500">
-      Not a member?
-      <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Start a 14 day free trial</a>
-    </p>
-  </div>
-</div>
-
+    </div>
 </body>
 </html>
--- resources/views/Components/nav-register.blade.php
+++ resources/views/Components/nav-register.blade.php
@@ -1,73 +1,69 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Registration</title>
-    <script src= "https://cdn.tailwindcss.com"></script>
+    <!-- Styles / Scripts -->
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @else
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <script src="{{ asset('js/app.js') }}" defer></script>
+    @endif
 </head>
-<body>
-    <!--
-  This example requires updating your template:
 
-  ```
-  <html class="h-full bg-white">
-  <body class="h-full">
-  ```
--->
-<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-  <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-    <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" class="mx-auto h-10 w-auto" />
-    <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">{{ $slot }}</h2>
-  </div>
-
-  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <form action="" method="POST" class="space-y-6">
-        @csrf
-
-        <div>
-        <label for="name" class="block text-sm/6 font-medium text-gray-900">Name</label>
-        <div class="mt-2">
-          <input id="name" type="text" name="name" required autocomplete="name" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+<body class="min-h-screen flex items-center justify-center bg-gray-50">
+    <div class="px-6 py-12 w-full max-w-lg content-center rounded-xl shadow-xl">
+        <div class="w-full">
+            <img src="/images/image.png" alt="Your Company" class="mx-auto h-10 w-auto" />
+            <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">{{ $slot }}</h2>
         </div>
 
-      <div>
-        <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
-        <div class="mt-2">
-          <input id="email" type="email" name="email" required autocomplete="email" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
-        </div>
-      </div>
+        <div class="mt-10 w-full">
+            <form action="" method="POST" class="space-y-6">
+                @csrf
 
-      <div>
-        <div class="flex items-center justify-between">
-          <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
-        </div>
-        <div class="mt-2">
-          <input id="password" type="password" name="password" required autocomplete="current-password" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
-        </div>
-      </div>
+                <div>
+                    <label for="name" class="block text-sm/6 font-medium text-gray-900">Name</label>
+                    <input type="text" id="name" name="name"
+                        class="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                        placeholder="John" required />
+                    <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
+                    <input id="email" type="email" name="email" required autocomplete="email"
+                        class="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
 
-      <div>
-        <div class="flex items-center justify-between">
-          <label for="password_confirmation" class="block text-sm/6 font-medium text-gray-900">Confirm Password</label>
-        </div>
-        <div class="mt-2">
-          <input id="password_confirmation" type="password" name="password_confirmation" required autocomplete="current-password" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
-        </div>
-      </div>
+                    <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
+                    <input id="password" type="password" name="password" required autocomplete="current-password"
+                        class="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
 
-      <div class="mt-6">
-        <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">S'inscrire</button>
-      </div>
-    </form>
+                    <label for="password_confirmation" class="block text-sm/6 font-medium text-gray-900">Confirm
+                        Password</label>
+                    <input id="password_confirmation" type="password" name="password_confirmation" required
+                        autocomplete="current-password"
+                        class="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+                </div>
 
-    <p class="mt-10 text-center text-sm/6 text-gray-500">
-      Not a member?
-      <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Start a 14 day free trial</a>
-    </p>
-  </div>
-</div>
+                <div class="mt-6">
+                    <button type="submit"
+                        class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">S'inscrire</button>
+                </div>
+            </form>
+
+            <p class="mt-10 text-center text-sm/6 text-gray-500">
+                Already have an account?
+                <a href="/auth/login" class="font-semibold text-indigo-600 hover:text-indigo-500">Sign in</a>
+            </p>
+            <p class="mt-2 text-center text-sm/6 text-gray-500">
+                Not a member?
+                <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Start a 14 day free
+                    trial</a>
+            </p>
+        </div>
+    </div>
 
 </body>
+
 </html>
